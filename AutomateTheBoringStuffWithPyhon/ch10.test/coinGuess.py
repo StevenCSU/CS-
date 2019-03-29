f51d969e15f611dee097d02de5d84ce76bd5e7cf
Steven Z <stevencsu@163.com>
2019-03-30 01:14:15 +0800

    add ch10 test

---
+++ AutomateTheBoringStuffWithPyhon/ch10.test/coinGuess.py
@@ -0,0 +1,26 @@
+import random
+import logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of the game.')
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+
+toss = random.randint(0, 1)
+logging.debug('the coin is there. %d' % toss)
+if toss:
+    toss = 'heads'
+else:
+    toss = 'tails'
+logging.debug('the word is %s.' % toss)
+
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
